a127f59f6558031487030f4feeaeab669b196273
howlrs <sharebook.amazon@gmail.com>
2025-01-06 17:35:58 +0900

    google-site-verify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,8 @@ export const metadata: Metadata = {
   title: "[GeminiAI生成] 講義と漫才で学ぶ算数数学",
   description: "講義と漫才で学ぶ算数数学です。Google Gemini AIが各数学公式を講義、漫才形式で出力しました。自分にあった出力を選択し算数や数学を学ぶことができます。",
   verification: {
-    // <meta name="google-site-verification" content="" />
-    google: "",
+    // <meta name="google-site-verification" content="bZzBB_NIdeSkdU-dHrIlcf_Z9rr969I4pSxBFXcyUMQ" />
+    google: "bZzBB_NIdeSkdU-dHrIlcf_Z9rr969I4pSxBFXcyUMQ",
   },
 };
 
